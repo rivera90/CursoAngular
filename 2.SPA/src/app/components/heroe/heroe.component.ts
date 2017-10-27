17302ebfd2d6861f26343c4fac0d584c78bd5c3c
Armando <riveraarmando90@gmail.com>
2017-10-27 09:55:38 -0600

    primer commit

---
+++ 2.SPA/src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import {HeroesService,Heroe} from '../../servicios/heroes.service';
+
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent {
+
+  heroe:any = {};
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService
+   ) {
+
+      this.activatedRoute.params.subscribe(params => {
+        this.heroe = this._heroesService.getHeroe(params['id']);
+        console.log(this.heroe);
+      });
+
+   }
+
+}
